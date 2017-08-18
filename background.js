55841326603ee2ec357ae6fc93faa14481547453
Jessy James <mcdl@tutanota.com>
2017-08-18 13:58:14 +0200

    Migrate to WebExtensions
    Firefox 57 will be released in November 2018. That version won't support
    old plugins any more. This commit converts the plugin into the supported
    WebExtensions format.

---
+++ background.js
@@ -0,0 +1,29 @@
+var urlPattern = 'https?://([^\.]+\.)?mixcloud.com(/[^/]+/[^/]+/?)';
+
+browser.contextMenus.create({
+    id: "mixcloud-downloader",
+    title: "Download Mix",
+    contexts: ["all"],
+    documentUrlPatterns: [
+        "*://*.mixcloud.com/*/*",
+        "*://mixcloud.com/*/*"
+    ]
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if(info.menuItemId != "mixcloud-downloader") {
+        return;
+    }
+    
+    console.log(tab.url);
+    var regExp = new RegExp(urlPattern, "i");
+    var matches = regExp.exec(tab.url);
+    if (matches == null) {
+        console.error("Couldn't extract key from " + tab.url);
+        return;
+    }
+    var downloadUrl = "http://www.mixcloud-downloader.com" + matches[2] + "?utm_source=firefox-extension";
+    browser.tabs.create({
+        url: downloadUrl
+    });
+});
